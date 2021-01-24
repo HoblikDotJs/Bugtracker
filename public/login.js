301c33f299cda1eb66123f5a4d200f1fe05342fb
hoblikDotJs <hoblik.js@gmail.com>
2021-01-24 19:10:46 +0100

    stages css

---
+++ public/login.js
@@ -0,0 +1,59 @@
+let signupToggle = false;
+
+function signupShow() {
+    if (signupToggle) {
+        $("#loginButton").html("Log in");
+        $("#heading").html("Log In");
+        $("#signupToggle").html("Signup now");
+        $("#memberToggle").html("Not a member?");
+        signupToggle = false;
+    } else {
+        $("#loginButton").html("Sign Up");
+        $("#heading").html("Sign Up");
+        $("#signupToggle").html("Log In");
+        $("#memberToggle").html("I am already a member");
+        signupToggle = true;
+    }
+}
+
+function login() {
+    const email = $("#email").val();
+    const password = $("#pass").val();
+    if (signupToggle) {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+            .then((userCredential) => {
+                // Signed in 
+                signedIn(userCredential.user);
+            })
+            .catch((error) => {
+                const errorMessage = error.message;
+                console.log(errorMessage);
+            });
+    } else {
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .then((userCredential) => {
+                // Signed in 
+                signedIn(userCredential.user);
+            })
+            .catch((error) => {
+                const errorMessage = error.message;
+                console.log(errorMessage);
+            });
+    }
+}
+
+function signedIn(_) {
+    $("#loginForm").hide();
+    $("#logoutButton").html("Log Out")
+    const user = _;
+    const UID = user.uid
+    console.log("success", UID);
+}
+
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId());
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail());
+}
