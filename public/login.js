4b2923cb546df0f0da60b7a4f5a3c733f95e2a99
hoblikDotJs <hoblik.js@gmail.com>
2021-01-28 20:53:01 +0100

    drag and drop

--- public/login.js
+++ public/login.js
@@ -1,3 +1,53 @@
+function signedIn(_) {
+    $("#loginForm").hide();
+    $("#logoutButton").html("Log Out")
+    const user = _;
+    const UID = user.uid
+    console.log("success", UID);
+}
+
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId());
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail());
+    let img = profile.getImageUrl();
+    preloadImage(img);
+    sett.user = img;
+    $("#googleBtn").html(`<img id="userImg" src="${img}">`);
+    $("#someuserimg").html(`<img class="otherUsersImages" src="${img}">`)
+}
+let myImage;
+
+function preloadImage(url) {
+    myImage = new Image();
+    myImage.className = "otherUsersImages";
+    myImage.src = url;
+}
+
+/*
+function preloadImage(url) {
+    if (localStorage.getItem("myImg") == null) {
+        myImage = new Image();
+        myImage.className = "otherUsersImages";
+        console.log("using fetched");
+        myImage.src = url;
+        localStorage.setItem("myImg", myImage)
+    } else {
+        console.log("using preloaded");
+        myImage = localStorage.getItem("myImg");
+    }
+}*/
+
+
+
+
+
+
+
+
+/*
 let signupToggle = false;
 
 function signupShow() {
@@ -15,7 +65,6 @@ function signupShow() {
         signupToggle = true;
     }
 }
-
 function login() {
     const email = $("#email").val();
     const password = $("#pass").val();
@@ -41,21 +90,4 @@ function login() {
             });
     }
 }
-
-function signedIn(_) {
-    $("#loginForm").hide();
-    $("#logoutButton").html("Log Out")
-    const user = _;
-    const UID = user.uid
-    console.log("success", UID);
-}
-
-function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId());
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail());
-    $("#googleBtn").html(`<img id="userImg" src="${profile.getImageUrl()}">`);
-    $("#someuserimg").html(`<img class="otherUsersImages" src="${profile.getImageUrl()}">`)
-}
+*/
--- public/main.js
+++ public/main.js
@@ -1,35 +1,61 @@
+const sett = {
+  text: "Enter text here",
+  user: "https://lh3.googleusercontent.com/a-/AOh14Ghy3XMxqrfkEkrbBkRyfZwzEZhT76BQxqGopay4=s96-c",
+  priority: "low"
+};
+
 function addTodo() {
-    console.log("todo")
-    $("#todoBoxes").append(`<div class="box">
-    <p>Cant select your mother Cant select your mother Cant select your mother</p>
-    <div id="someuserimg"></div>
-    <p class="lowPriority">Low</p>
-  </div>`)
+  console.log("todo")
+  $("#todoBoxes").append(createTile(sett))
+  updateDragging()
 }
 
 function addInprogress() {
-    console.log("progress")
-    $("#progressBoxes").append(`<div class="box">
-    <p>Cant select your mother Cant select your mother Cant select your mother</p>
-    <div id="someuserimg"></div>
-    <p class="lowPriority">Low</p>
-  </div>`)
+  console.log("progress")
+  $("#progressBoxes").append(createTile(sett))
+  updateDragging()
 }
 
 function addReview() {
-    console.log("review")
-    $("#reviewBoxes").append(`<div class="box">
-    <p>Cant select your mother Cant select your mother Cant select your mother</p>
-    <div id="someuserimg"></div>
-    <p class="lowPriority">Low</p>
-  </div>`)
+  console.log("review")
+  $("#reviewBoxes").append(createTile(sett))
+  updateDragging()
 }
 
 function addDone() {
-    console.log("done")
-    $("#doneBoxes").append(`<div class="box">
-    <p>Cant select your mother Cant select your mother Cant select your mother</p>
-    <div id="someuserimg"></div>
-    <p class="lowPriority">Low</p>
-  </div>`)
+  console.log("done")
+  $("#doneBoxes").append(createTile(sett))
+  updateDragging()
+}
+
+function createTile({
+  user,
+  text,
+  priority
+}) {
+  return `<div class="draggable"  draggable="true">
+    <div contenteditable="true" onclick="editTile(this)">${text}</div>
+    ${myImage.outerHTML}
+    <p class="priority ${priority}Priority" onclick="changePriority(this)">Low</p>
+  </div>`
+}
+
+function editTile(el) {
+  window.getSelection().selectAllChildren(el);
+}
+
+function changePriority(el) {
+  if (el.classList[1] == "lowPriority") {
+    $(el).html("Medium")
+    $(el).removeClass("lowPriority");
+    $(el).addClass("mediumPriority");
+  } else if (el.classList[1] == "mediumPriority") {
+    $(el).html("High")
+    $(el).removeClass("mediumPriority");
+    $(el).addClass("highPriority");
+  } else if (el.classList[1] == "highPriority") {
+    $(el).html("Low")
+    $(el).removeClass("highPriority");
+    $(el).addClass("lowPriority");
+  }
 }
