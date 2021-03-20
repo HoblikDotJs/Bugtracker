c9e8a76b423ea8eda7fc062d19bbb6b7047ef7fd
hoblikDotJs <hoblik.js@gmail.com>
2021-03-20 18:17:38 +0100

    nothing

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.post('/loadProject', (req, res) => {
         } else { //NEW USER without i - create a new project, create profile
             console.log("NEW USER without i");
             data.profile.Projects = [];
-            const projectID = data.profile.ID.toString() + Date.parse(new Date).toString()
+            const projectID = uniqid()
             data.profile.Projects.push(projectID)
             DB.users[data.profile.ID] = data.profile;
             let project = {
@@ -133,7 +133,7 @@ app.post('/showProjects', (req, res) => {
 app.post('/createProject', (req, res) => {
     console.log("creating project");
     const ID = req.body.ID;
-    const projectID = ID.toString() + Date.parse(new Date).toString()
+    const projectID = uniqid()
     DB.users[ID].Projects.push(projectID);
     let project = {
         "data": {
